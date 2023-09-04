401133bfc10229070fea00b854a90d9907de2f74
Abd ul Walee Talal <aboodtalal51@gmail.com>
2023-09-04 20:56:53 +0300

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("connected to mogodb")
+}).catch((e) => {
+  console.log(e)
+
+})
 const app = express();
 app.listen(3000, () => {
   console.log("server is listning on port 3000 ")
