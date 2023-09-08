1e026fb81071f64127520318352c7ff498a84442
Abd ul Walee Talal <aboodtalal51@gmail.com>
2023-09-08 22:26:33 +0300

    creat sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,3 +16,20 @@ export const signup = async (req, res, next) => {
   }
 
 };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email })
+    if (!validUser) return next(errorHandler(404, 'User not found'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Worng credntails'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)//seriesBlack
+    const { password: hashPassword, ...rest } = validUser._doc;
+    const expires = new Date(Date.now() + 3600000); //1 houre for cookie to expire
+    res.cookie('access_token', token, { httpOnly: true, expires }).status(200).json(rest)
+
+  } catch (error) {
+    next(error);
+  }
+}
