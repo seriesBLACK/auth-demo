3438250a51db53347c3ed4cc063656b5f399489e
Abd ul Walee Talal <aboodtalal51@gmail.com>
2023-09-11 00:25:36 +0300

    Profile page UI

--- clint/src/pages/Profile.jsx
+++ clint/src/pages/Profile.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import '../css files/profiel.css';
 
 export default function Profile() {
+  const { currentUser } = useSelector(state => state.user);
+
+
+
   return (
-    <div>
-      profile
-    </div>
+    <form className='profilePage signUp-form'>
+      <h1 className='profielh1'>Profile</h1>
+      <img className='currentUserUrl' src={currentUser.photo} alt="" />
+      <input type="text" placeholder={currentUser.username} />
+      <input type="text" placeholder={currentUser.email} />
+      <input type="password" placeholder='password' />
+      <button className='updateBtn'>UPDATE</button>
+      <div className='delete-signOut'>
+        <p>Delete account</p>
+        <p>Sign out</p>
+      </div>
+    </form>
   )
 }
